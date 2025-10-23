298004a4f8b27fb356ee5a40458195cb85a01f29
FawazALhafiz <fawaztwix111@gmail.com>
2025-10-23 06:32:06 +0300

    chore: fetch pokemons at once

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <ul>
+      <li v-for="pokemon in pokemoon" :key="pokemon.id">{{ pokemon.name }}</li>
+    </ul>
+  </div>
 </template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const pokemoon = ref([]);
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 10; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        console.log(results);
+        pokemoon.value = results;
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
+
+
+</script>
+
 
 <style scoped></style>
