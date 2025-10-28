60c20b90d80baa1a5dba234139b79ea8b08302ef
FawazALhafiz <fawaztwix111@gmail.com>
2025-10-28 19:20:14 +0300

    feat: creating ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error-container">
+    <div class="error-icon">⚠️</div>
+    <p class="error-message">{{ message }}</p>
+    <button v-if="onRetry" @click="onRetry" class="retry-button">
+      Try Again
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  message: {
+    type: String,
+    required: true
+  },
+  onRetry: {
+    type: Function,
+    default: null
+  }
+});
+</script>
