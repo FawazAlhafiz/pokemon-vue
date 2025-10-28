905e8c398ea6730072435ba9cb7ba0a4f220aac7
FawazALhafiz <fawaztwix111@gmail.com>
2025-10-28 19:13:24 +0300

    feat: creating PokemonGrig component

---
+++ src/components/PokemonGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="pokemon-grid">
+    <PokemonCard 
+      v-for="pokemon in pokemonList" 
+      :key="pokemon.id" 
+      :pokemon="pokemon"
+    />
+  </ul>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+  import PokemonCard from './PokemonCard.vue';
+
+  defineProps({
+    pokemonList: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  });
+</script>
