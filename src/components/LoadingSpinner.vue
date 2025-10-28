9f2937f60d6db9dba8cf6cfc195f793cd76eb8ab
FawazALhafiz <fawaztwix111@gmail.com>
2025-10-28 19:15:27 +0300

    feat: creating LoadingSpinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="loading-container">
+    <div class="spinner"></div>
+    <p class="loading-text">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    default: 'Loading...'
+  }
+});
+</script>
